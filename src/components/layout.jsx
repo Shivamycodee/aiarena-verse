19eefbb18a40d0333e2e2b676d28ce40e3d85aee
Shivamycodee <91057727+Shivamycodee@users.noreply.github.com>
2023-10-10 11:20:38 +0530

    UI updated with phone UI, and scroll bar

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -3,11 +3,7 @@ import Navbar from "./navbarComp";
 function Layout({ children }) {
 
   return (
-    <div
-      style={{
-        background: "linear-gradient(to bottom, #e3eeff,#7e9ff2,#f3e7e9)",
-      }}
-    >
+    <div>
       <Navbar />
       {children}
     </div>
